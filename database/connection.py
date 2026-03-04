1ab84c6972caf3510fe6945d080c9e04d3284758
unknown <shfhanny@gmail.com>
2026-03-04 09:31:49 +0700

    chore: differentiate read and write connecrtion

--- database/connection.py
+++ database/connection.py
@@ -11,17 +11,21 @@ from dotenv import load_dotenv
 
 load_dotenv()  # LOAD ENV
 
+# SET GLOBAL SSH TUNNEL
+tunnel = sshtunnel.SSHTunnelForwarder(
+    (os.getenv("SSH_HOST"), int(os.getenv("SSH_PORT"))),
+    ssh_username=os.getenv("SSH_USERNAME"),
+    ssh_password=os.getenv("SSH_PASSWORD"),
+    remote_bind_address=(os.getenv("DB_HOST"), int(os.getenv("DB_PORT"))),
+)
 
-def get_db_connection():
-    # CONENCTION VIA SSH TUNNEL
-    tunnel = sshtunnel.SSHTunnelForwarder(
-        (os.getenv("SSH_HOST"), int(os.getenv("SSH_PORT"))),
-        ssh_username=os.getenv("SSH_USERNAME"),
-        ssh_password=os.getenv("SSH_PASSWORD"),
-        remote_bind_address=(os.getenv("DB_HOST"), int(os.getenv("DB_PORT"))),
-    )
-    tunnel.start()
-    conn = sql.connect(
+tunnel.start()
+print(f"[SSH] Tunnel started at localhost: {tunnel.local_bind_port}")
+
+
+# READ CONNECTION
+def get_read_connection():
+    return sql.connect(
         user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
@@ -30,4 +34,20 @@ def get_db_connection():
         autocommit=True,
     )
 
-    return conn, tunnel
+
+# WRITE CONNECTION
+def get_write_connection():
+    return sql.connect(
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=tunnel.local_bind_port,
+        database=os.getenv("DB_NAME"),
+        autocommit=False,
+    )
+
+
+# CLOSE TUNNEL
+def close_tunnel():
+    tunnel.stop()
+    print("[SSH] Tunnel closed!")
